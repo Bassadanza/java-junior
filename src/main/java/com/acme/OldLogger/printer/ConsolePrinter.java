cee5c4270883c94d4f3182d61923c411f400ea07
Dmitry <toropindmitriy@gmail.com>
2018-03-10 00:08:37 +0300

    OldLogger deleted

--- src/main/java/com/acme/OldLogger/printer/ConsolePrinter.java
+++
@@ -1,9 +0,0 @@
-package com.acme.OldLogger.printer;
-
-public class ConsolePrinter implements Printer {
-
-    @Override
-    public void print(final Object message) {
-        System.out.println(message.toString());
-    }
-}
--- src/main/java/com/acme/OldLogger/printer/Printer.java
+++
@@ -1,5 +0,0 @@
-package com.acme.OldLogger.printer;
-
-public interface Printer {
-    void print(Object message);
-}
